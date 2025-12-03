22c89ca0253019b0d0bed06cadf69ddc44d381ac
BlueVelociraptor <bluevelociraptor1555@gmail.com>
2025-12-03 12:13:44 -0500

    cors actualizado

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,27 @@
 import { CorsOptions } from "cors"
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 
-const whitelist = [process.env.FRONTEND_URL, 'http://localhost:18512'];
+const whitelist = [
+    process.env.FRONTEND_URL, 
+    'http://localhost:18512', 
+    'http://127.0.0.1:18512', 
+];
 
 export const corsOptions: CorsOptions = {
     origin: function (origin: string | undefined, callback) {
-        if (whitelist.indexOf(origin ?? '') !== -1 || !origin) {
-            callback(null, true)
+        // Si no hay origen (peticiones desde Postman, curl, etc.)
+        if (!origin) {
+            return callback(null, true);
+        }
+        
+        // Verificar si el origen está en la whitelist
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'))
+            console.log(`CORS bloqueado para origen: ${origin}`);
+            callback(new Error('Not allowed by CORS'));
         }
     }
 }
